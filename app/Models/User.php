0eac3e2ca9949cf0daa10a6232fc7f274b5a76f4
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-25 19:18:30 +0100

    Move User

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,22 +1,21 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'username',
+        'email',
+        'password',
     ];
 
     /**
