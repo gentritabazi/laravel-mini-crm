a0fd854d68202f7fab9a105a4e1773a88a2db917
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-25 21:20:27 +0100

    Added folders in models

--- app/Models/Companies/Company.php
+++ app/Models/Companies/Company.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Companies;
 
 use App\Abstracts\Model;
 
--- app/Repositories/Companies/CompanyRepository.php
+++ app/Repositories/Companies/CompanyRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repositories\Companies;
 
-use App\Models\Company;
+use App\Models\Companies\Company;
 use App\Abstracts\Repository;
 
 class CompanyRepository extends Repository
