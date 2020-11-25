d073518bc328b961f2d6258c719ac9212eb55c39
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-25 21:11:58 +0100

    Added component no-pagination

---
+++ resources/views/components/no-pagination.blade.php
@@ -0,0 +1,11 @@
+<ul role="navigation" class="pagination">
+    <li class="page-item disabled">
+        <a href="#" rel="prev" class="page-link">‹</a>
+    </li>
+    <li aria-current="page" class="page-item disabled">
+        <span class="page-link">1</span>
+    </li>
+    <li aria-disabled="true" aria-label="Next »" class="page-item disabled">
+        <span aria-hidden="true" class="page-link">›</span>
+    </li>
+</ul>
