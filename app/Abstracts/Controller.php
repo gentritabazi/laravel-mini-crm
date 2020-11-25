59e6f1473a389e9e7c723f4cef63bafd91f59b16
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-25 19:10:43 +0100

    Move default controller

--- app/Abstracts/Controller.php
+++ app/Abstracts/Controller.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Abstracts;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
