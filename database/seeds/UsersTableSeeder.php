f40f9aec866fbb51e4788f082a490913926eb850
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-25 19:11:15 +0100

    Created UsersTableSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $row = [
+            'name' => 'Kutia',
+            'username' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'remember_token' => str_random(10),
+        ];
+
+        User::create($row);
+    }
+}
