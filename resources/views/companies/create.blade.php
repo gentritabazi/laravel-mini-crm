f81402cd81e5b28392c463993dd25a4a43cc2568
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-26 21:41:11 +0100

    Added readImage

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -21,8 +21,8 @@
 
                             <div class="text-center mb-4">
                                 <div style="width: 30%; margin: 0 auto;">
-                                    <img width="100" height="100" src="{{ asset('images/no-image-available.png') }}">
-                                    <input type="file" name="logo" class="form-control mt-4">
+                                    <img id="file-preview" width="100" height="100" src="{{ asset('images/no-image-available.png') }}">
+                                    <input id="file" type="file" name="logo" class="form-control mt-4" accept="image/*">
                                 </div>
                             </div>
 
@@ -56,4 +56,32 @@
             </div>
         </div>
     </div>
+
+    <script>
+        function readImage(input) {
+            const noImageUrl = "{{ asset('images/no-image-available.png') }}";
+
+            if(!input.files) {
+                return $('#file-preview').attr('src', noImageUrl);
+            }
+
+            if (input.files[0]) {
+                const reader = new FileReader();
+                
+                reader.onload = function(e) {
+                    $('#file-preview').attr('src', e.target.result);
+                }
+                
+                reader.readAsDataURL(input.files[0]);
+            } else {
+                return $('#file-preview').attr('src', noImageUrl);
+            }
+        }
+
+        $(function() {
+            $('#file').change(function() {
+                readImage(this);
+            });
+        });
+    </script>
 @endsection
