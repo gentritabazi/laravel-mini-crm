f003c34e6bfdf3ce7fe43e89bad1f73c45b6374c
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-26 21:40:29 +0100

    Added jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
